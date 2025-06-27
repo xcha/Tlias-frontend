6aaeaf3b1b8d1ea951cfd24a1005ae8dc8ae712f
zdx <1123951395@qq.com>
2025-06-27 14:36:02 +0800

    feat: 初始化项目结构并添加基础功能模块
    - 添加项目基础文件结构及配置
    - 实现登录页面和主布局框架
    - 添加部门管理、学员管理等功能模块
    - 配置路由系统实现页面导航
    - 集成Element Plus UI组件库

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+//引入views/layout/index.vue命名为Layout
+import Layout from "@/views/layout/index.vue";
+import router from "./router";
+</script>
+
+<template>
+  <router-view></router-view>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/views/dept/index.vue
@@ -0,0 +1,46 @@
+<script setup>
+import { ref ,onMounted} from 'vue';
+import axios from 'axios'
+
+onMounted(() => {
+  search()
+})
+const search = async () => {
+  const result = await axios.get('https://m1.apifoxmock.com/m1/6430447-6127879-default/detps')
+  if (result.data.code) {
+    deptList.value = result.data.data
+  }
+}
+
+
+const deptList = ref([])
+</script>
+
+<template>
+  <h1>部门管理</h1>
+  <div class="container">
+    <el-button type="primary"> + 新增部门</el-button>
+
+  </div>
+  <div>
+    <el-table :data="deptList" border style="width: 100%">
+      <el-table-column type="index" width="100" align="center" />
+      <el-table-column prop="name" label="部门名称" width="260" align="center"/>
+      <el-table-column prop="updateTime" label="最后操作时间" width="300" align="center"/>
+      <el-table-column label="操作"align="center">
+        <template #default="scope">
+          <el-button type="primary" size="small"> <el-icon><Edit /></el-icon>编辑</el-button>
+          <el-button type="danger" size="small"> <el-icon><Delete /></el-icon>删除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+  </div>
+
+
+</template>
+
+<style scoped>
+.container {
+  margin: 10px 0px;
+}
+</style>
