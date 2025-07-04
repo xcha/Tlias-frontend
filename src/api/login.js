f3f4f83e21e76e439ae71034b08a4c4210956f0b
zdx <1123951395@qq.com>
2025-07-04 09:36:40 +0800

    feat(用户): 添加修改密码功能及对话框
    - 在api/login.js中添加修改密码接口updatePasswordApi
    - 在layout/index.vue中实现修改密码对话框及表单验证
    - 添加密码修改相关逻辑和UI组件

--- src/api/login.js
+++ src/api/login.js
@@ -2,3 +2,12 @@ import request from '@/utils/request'
 
 //登录
 export const loginApi = (data) => request.post('/login', data)
+
+// src/api/login.js 中添加
+export const updatePasswordApi = (data) => {
+  return request({
+    url: '/emps',
+    method: 'put',
+    data
+  })
+}
--- src/views/layout/index.vue
+++ src/views/layout/index.vue
@@ -1,19 +1,95 @@
 <script setup>
-import { ref, onMounted } from 'vue'
-import { ElMessage, ElMessageBox } from 'element-plus'
+import { ref, onMounted, reactive } from 'vue'
+import { ElMessage, ElMessageBox, ElForm, ElFormItem, ElInput, ElDialog } from 'element-plus'
 import { useRouter } from 'vue-router'
+// 导入图标
+import { EditPen, SwitchButton, Promotion, Menu, HomeFilled, UserFilled, Tools, HelpFilled, Avatar, Histogram, InfoFilled, Share, Document } from '@element-plus/icons-vue'
 
 const loginName = ref('')
 const router = useRouter()
 
+// 添加修改密码相关变量
+const showPasswordDialog = ref(false)
+const passwordFormRef = ref(null)
+const passwordForm = reactive({
+  oldPassword: '',
+  newPassword: '',
+  confirmPassword: ''
+})
+
+// 密码验证规则
+const passwordRules = {
+  oldPassword: [
+    { required: true, message: '请输入旧密码', trigger: 'blur' },
+    { min: 6, message: '密码长度不能少于6位', trigger: 'blur' }
+  ],
+  newPassword: [
+    { required: true, message: '请输入新密码', trigger: 'blur' },
+    { min: 6, message: '密码长度不能少于6位', trigger: 'blur' }
+  ],
+  confirmPassword: [
+    { required: true, message: '请确认新密码', trigger: 'blur' },
+    { min: 6, message: '密码长度不能少于6位', trigger: 'blur' },
+    {
+      validator: (rule, value, callback) => {
+        if (value !== passwordForm.newPassword) {
+          callback(new Error('两次输入密码不一致!'))
+        } else {
+          callback()
+        }
+      },
+      trigger: 'blur'
+    }
+  ]
+}
+
 onMounted(() => {
-  const loginUser = loginName.value = JSON.parse(localStorage.getItem('loginUser'))
+  const loginUser = JSON.parse(localStorage.getItem('loginUser'))
   if (loginUser) {
     loginName.value = loginUser.name
   }
 })
 
+// 打开修改密码对话框
+const openPasswordDialog = () => {
+  showPasswordDialog.value = true
+  // 重置表单
+  if (passwordFormRef.value) {
+    passwordFormRef.value.resetFields()
+  }
+}
 
+// 提交密码修改
+const submitPasswordChange = async () => {
+  if (!passwordFormRef.value) return
+  try {
+    await passwordFormRef.value.validate()
+    const result = await updatePasswordApi({
+      // 获取表单数据 新的密码的值
+      id: loginUser.id,
+      password: passwordForm.value.password
+    })
+    if (result.code) {
+      ElMessage.error(result.msg)
+      return
+    }
+
+    // 模拟API请求
+    console.log('修改密码参数:', {
+      userId: loginUser.id,
+      oldPassword: passwordForm.oldPassword,
+      newPassword: passwordForm.newPassword
+    })
+
+    // 假设API调用成功
+    ElMessage.success('密码修改成功，请重新登录')
+    showPasswordDialog.value = false
+    // 密码修改成功后退出登录
+    router.push('/login')
+  } catch (error) {
+    ElMessage.error('表单验证失败，请检查输入')
+  }
+}
 const logout = () => {
   ElMessageBox.confirm('是否退出登录？', '提示',
     {
@@ -27,7 +103,6 @@ const logout = () => {
         type: 'success',
         message: '退出登录成功',
       })
-      localStorage.removeItem('loginUser')
       router.push('/login')
     })
     .catch(() => {
@@ -38,6 +113,8 @@ const logout = () => {
     })
 }
 
+
+
 </script>
 
 <template>
@@ -47,7 +124,7 @@ const logout = () => {
       <el-header class="header">
         <span class="title">Tlias智能学习辅助系统</span>
         <span class="right_tool">
-          <a href="">
+          <a href="javascript:void(0)" @click="openPasswordDialog">
             <el-icon>
               <EditPen />
             </el-icon> 修改密码 &nbsp;&nbsp;&nbsp; | &nbsp;&nbsp;&nbsp;
@@ -135,13 +212,41 @@ const logout = () => {
           </el-menu>
         </el-aside>
 
+
+
+
         <el-main>
           <router-view></router-view>
         </el-main>
       </el-container>
 
+
     </el-container>
+
+
   </div>
+
+  <el-dialog v-model="showPasswordDialog" title="修改密码" width="30%">
+    <h2 style="color: red;">"修改密码功能维护中...暂时不可用!"</h2>
+    <el-form :model="passwordForm" :rules="passwordRules" ref="passwordFormRef" label-width="100px">
+      <el-form-item label="旧密码" prop="oldPassword">
+        <el-input v-model="passwordForm.oldPassword" type="password" autocomplete="off" />
+      </el-form-item>
+      <el-form-item label="新密码" prop="newPassword">
+        <el-input v-model="passwordForm.newPassword" type="password" autocomplete="off" />
+      </el-form-item>
+      <el-form-item label="确认密码" prop="confirmPassword">
+        <el-input v-model="passwordForm.confirmPassword" type="password" autocomplete="off" />
+      </el-form-item>
+    </el-form>
+    <template #footer>
+      <span class="dialog-footer">
+        <el-button @click="showPasswordDialog = false">取消</el-button>
+        <el-button type="primary" @click="submitPasswordChange">确认</el-button>
+      </span>
+    </template>
+  </el-dialog>
+
 </template>
 
 <style scoped>
